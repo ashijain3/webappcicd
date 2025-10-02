f76aaacbd5ba07ec9e327571f47e3dba937e74ff
ashijain3 <ashijain3@users.noreply.github.com>
2025-10-02 16:14:02 +0530

    added path for index.html

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,16 @@
+import os
 from flask import Flask, render_template, jsonify
 
-app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # project root
+TEMPLATE_DIR = os.path.join(BASE_DIR, "frontend", "templates")
+STATIC_DIR = os.path.join(BASE_DIR, "frontend", "static")
 
-# Home page -> load frontend (index.html)
+app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# Simple API endpoint
 @app.route("/api/message")
 def get_message():
     return jsonify({"message": "Hello from Flask Backend!"})
